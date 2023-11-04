c1c4c205e0e6c4db5fba066d7fc3cd5b40c0fc03
petarpandza <petarpandzapetar@gmail.com>
2023-11-04 12:46:41 +0100

    Small syntax fix

--- database/serialize.js
+++ database/serialize.js
@@ -1,5 +1,5 @@
 const db = require('./db');
-const user = require('../models/users');
+const user = require('../models/user');
 const parking_space = require('../models/parking_space');
 const reservation = require('../models/reservation');
 const user_vehicle = require('../models/user_vehicle');
@@ -7,7 +7,7 @@ const user_vehicle = require('../models/user_vehicle');
 function serialise(){
     db.serialize(() => {
         db.run(user.createUserTable);
-        db.run(user.addUser, ["Admin", "Admin", "admin@fer.hr", "password", 1, null])
+        db.run(user.addUser, ["Admin", "Admin", "admin@gmail.hr", "password", 1, null])
         db.run(user.addUser, ["Alen", "Bazant", "alen.bazant@fer.hr", "password", 0, null])
         db.run(parking_space.createParkingSpaceTable);
         db.run(reservation.createReservationTable);
