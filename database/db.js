6f85a1304c5e9d4c2147676dc2ed47aeee8809be
NinoNogic <11751305+nogicn@users.noreply.github.com>
2023-11-05 01:47:36 +0100

    debug on

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database('./database/parking.db', (err) => {
+let db = new sqlite3.Database(':memory:', (err) => {
   if (err) {
     console.error(err.message);
   }
